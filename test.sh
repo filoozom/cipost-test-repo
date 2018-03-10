59337be62b55f091fb7aab33711f8398323a3973
Philippe Schommers <philippe@schommers.be>
2018-03-10 16:08:04 +0100

    Add pull request check

--- test.sh
+++ test.sh
@@ -1,3 +1,7 @@
 #/bin/sh
 
-curl -X POST -F "file=@file1.zip" -F "file=@file2.zip" http://srv01.apyos.com:4000/travis/artifacts/$TRAVIS_PULL_REQUEST
+if [ $TRAVIS_PULL_REQUST != "false" ]
+then
+	echo "Uploading artifacts..."
+	curl -X POST -F "file=@file1.zip" -F "file=@file2.zip" http://srv01.apyos.com:4000/travis/artifacts/$TRAVIS_PULL_REQUEST
+fi
