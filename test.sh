2bd85d852b7497d8c43625554c97ce4b68b3ae90
Philippe Schommers <philippe@schommers.be>
2018-03-10 16:31:59 +0100

    Add PR number to test.sh

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@
 
 if [[ $TRAVIS_PULL_REQUEST != "false" ]]
 then
-	echo "Uploading artifacts..."
+	echo "Uploading artifacts ($TRAVIS_PULL_REQUEST)..."
 	curl -X POST -F "file=@file1.zip" -F "file=@file2.zip" http://srv01.apyos.com:4000/travis/artifacts/$TRAVIS_PULL_REQUEST
 fi
