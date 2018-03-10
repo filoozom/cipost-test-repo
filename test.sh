4d61938a3fbd5fa23aa1308d149355d5934aa20b
Philippe Schommers <philippe@schommers.be>
2018-03-10 16:34:24 +0100

    Fix test.sh

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@
 
 if [[ $TRAVIS_PULL_REQUEST != "false" ]]
 then
-	echo "Uploading artifacts ($TRAVIS_PULL_REQUEST)..."
-	curl -X POST -F "file=@file1.zip" -F "file=@file2.zip" http://srv01.apyos.com:4000/travis/artifacts/$TRAVIS_PULL_REQUEST
+	echo "Uploading artifacts..."
+	curl -X POST -F "file=@file1.zip" -F "file=@file2.zip" http://srv01.apyos.com:4000/travis/artifacts/$TRAVIS_BUILD_ID
 fi
