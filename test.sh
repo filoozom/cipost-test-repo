0b8a5eb3da0c0b99f3de5c66d5ed36c1f55fff5d
Philippe Schommers <philippe@schommers.be>
2018-03-10 16:15:24 +0100

    Fix bash script

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-if [[ $TRAVIS_PULL_REQUST != "false" ]]
+if [[ $TRAVIS_PULL_REQUEST != "false" ]]
 then
 	echo "Uploading artifacts..."
 	curl -X POST -F "file=@file1.zip" -F "file=@file2.zip" http://srv01.apyos.com:4000/travis/artifacts/$TRAVIS_PULL_REQUEST
