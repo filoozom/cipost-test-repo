bb7412686368dd7fe253a61a62ae57f86d228e31
Philippe Schommers <philippe@schommers.be>
2018-03-23 12:58:31 +0100

    Upgrade to production artipost.io

--- test.sh
+++ test.sh
@@ -3,5 +3,5 @@
 if [[ $TRAVIS_PULL_REQUEST != "false" ]]
 then
 	echo "Uploading artifacts..."
-	curl -X POST -F "file=@file1.zip" -F "file=@file2.zip" -F "comment=Files: [file1.zip], [file2.zip]" http://srv01.apyos.com:4000/travis/artifacts/$TRAVIS_BUILD_ID
+	curl -X POST -F "file=@file1.zip" -F "file=@file2.zip" -F "comment=Files: [file1.zip], [file2.zip]" https://artipost.io/travis/artifacts/$TRAVIS_BUILD_ID
 fi
